fbb7abe3193aec0bb9e1684caa414e648642ef92
kevalbarot <161421443+kevalbarot@users.noreply.github.com>
2024-05-29 07:31:50 +0530

    Update AssociativeCacheBenchmark.java

--- benchmarks/src/main/java/org/questdb/AssociativeCacheBenchmark.java
+++ benchmarks/src/main/java/org/questdb/AssociativeCacheBenchmark.java
@@ -1,27 +1,3 @@
-/*******************************************************************************
- *     ___                  _   ____  ____
- *    / _ \ _   _  ___  ___| |_|  _ \| __ )
- *   | | | | | | |/ _ \/ __| __| | | |  _ \
- *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
- *    \__\_\\__,_|\___||___/\__|____/|____/
- *
- *  Copyright (c) 2014-2019 Appsicle
- *  Copyright (c) 2019-2024 QuestDB
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- ******************************************************************************/
-
 package org.questdb;
 
 import io.questdb.metrics.Counter;
@@ -50,10 +26,18 @@ public class AssociativeCacheBenchmark {
     private static final Counter hitCounter = new CounterImpl("bench");
     private static final Counter missCounter = new CounterImpl("bench");
     private static final String[] queries = new String[N_QUERIES];
-    private final ConcurrentAssociativeCache<Integer> cacheNoMetrics;
-    private final ConcurrentAssociativeCache<Integer> cacheWithMetrics;
+    private ConcurrentAssociativeCache<Integer> cacheNoMetrics;
+    private ConcurrentAssociativeCache<Integer> cacheWithMetrics;
 
-    public AssociativeCacheBenchmark() {
+    static {
+        Rnd rnd = new Rnd();
+        for (int i = 0; i < queries.length; i++) {
+            queries[i] = Chars.toString(rnd.nextChars(100));
+        }
+    }
+
+    @Setup(Level.Trial)
+    public void setup() {
         final int cpus = Runtime.getRuntime().availableProcessors();
         cacheNoMetrics = new ConcurrentAssociativeCache<>(8 * cpus, 2 * cpus);
         cacheWithMetrics = new ConcurrentAssociativeCache<>(
@@ -129,13 +113,6 @@ public class AssociativeCacheBenchmark {
                     hitCounter,
                     missCounter
             );
-        }
-    }
-
-    static {
-        Rnd rnd = new Rnd();
-        for (int i = 0; i < queries.length; i++) {
-            queries[i] = Chars.toString(rnd.nextChars(100));
         }
     }
 }
